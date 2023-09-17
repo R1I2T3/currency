fcfaace9c36ab03b021e1a24815475f4c0710c20
Ritesh jha <jhar27018@gmail.com>
2023-09-17 12:38:12 +0530

    removing debudmode sticker and changing background color to white and enabeling safe area mode

--- lib/currency.dart
+++ lib/currency.dart
@@ -26,94 +26,100 @@ class _Currency extends State<Currency> {
       ),
       borderRadius: BorderRadius.all(Radius.circular(50)),
     );
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: const Text(
-          "Currency converter",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 30,
-            fontFamily: "sans-serif",
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          title: const Text(
+            "Currency converter",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+              fontFamily: "sans-serif",
+            ),
           ),
         ),
-      ),
-      body: ColoredBox(
-        color: const Color.fromARGB(200, 255, 215, 0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Text(
-                    "${result != 0 ? result.toStringAsFixed(2) : result.toString()} INR",
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                    child: TextField(
-                      cursorHeight: 20,
-                      controller: textEditingController,
-                      style: const TextStyle(color: Colors.black, fontSize: 25),
-                      decoration: const InputDecoration(
-                        hintText: "Enter amount in USD",
-                        hintStyle: TextStyle(
+        body: ColoredBox(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "${result != 0 ? result.toStringAsFixed(2) : result.toString()} INR",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
                           color: Colors.black,
-                        ),
-                        prefixIcon: Icon(Icons.monetization_on_outlined),
-                        prefixIconColor: Colors.black,
-                        filled: true,
-                        fillColor: Colors.amber,
-                        focusedBorder: border,
-                        enabledBorder: border,
-                      ),
-                      keyboardType: const TextInputType.numberWithOptions(),
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  Container(
-                    height: 80,
-                    padding: const EdgeInsets.all(10),
-                    child: TextButton(
-                      style: const ButtonStyle(
-                        shape: MaterialStatePropertyAll(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                      child: TextField(
+                        cursorHeight: 20,
+                        controller: textEditingController,
+                        style:
+                            const TextStyle(color: Colors.black, fontSize: 25),
+                        decoration: const InputDecoration(
+                          hintText: "Enter amount in USD",
+                          hintStyle: TextStyle(
+                            color: Colors.black,
+                          ),
+                          prefixIcon: Icon(Icons.monetization_on_outlined),
+                          prefixIconColor: Colors.black,
+                          filled: true,
+                          fillColor: Colors.amber,
+                          focusedBorder: border,
+                          enabledBorder: border,
+                        ),
+                        keyboardType: const TextInputType.numberWithOptions(),
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(10),
+                      child: TextButton(
+                        style: const ButtonStyle(
+                          shape: MaterialStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                            ),
+                          ),
+                          elevation: MaterialStatePropertyAll(10.0),
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.black),
+                          foregroundColor:
+                              MaterialStatePropertyAll(Colors.amber),
+                          minimumSize: MaterialStatePropertyAll(
+                              Size(double.infinity, 50)),
+                        ),
+                        onPressed: () => {
+                          setState(() {
+                            if (textEditingController.text == "") {
+                              result = 0;
+                            }
+                            result =
+                                double.parse(textEditingController.text) * 81;
+                          })
+                        },
+                        child: const Text(
+                          "Convert",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.amber,
                           ),
                         ),
-                        elevation: MaterialStatePropertyAll(10.0),
-                        backgroundColor: MaterialStatePropertyAll(Colors.black),
-                        foregroundColor: MaterialStatePropertyAll(Colors.amber),
-                        minimumSize:
-                            MaterialStatePropertyAll(Size(double.infinity, 50)),
-                      ),
-                      onPressed: () => {
-                        setState(() {
-                          if (textEditingController.text == "") {
-                            result = 0;
-                          }
-                          result =
-                              double.parse(textEditingController.text) * 81;
-                        })
-                      },
-                      child: const Text(
-                        "Convert",
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.amber,
-                        ),
                       ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
